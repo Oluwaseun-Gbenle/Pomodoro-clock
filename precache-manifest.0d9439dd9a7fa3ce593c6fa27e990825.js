1a44c5d766ef4f61a7226504a5078932cb7df667
Oluwaseun-Gbenle <62512861+Oluwaseun-Gbenle@users.noreply.github.com>
2020-07-11 17:26:00 +0100

    Updates

---
+++ precache-manifest.0d9439dd9a7fa3ce593c6fa27e990825.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b64aa82d84917ce4006941a306ce57d",
+    "url": "/Pomodoro-clock/index.html"
+  },
+  {
+    "revision": "39124f8a11c942c7cb78",
+    "url": "/Pomodoro-clock/static/css/main.99e26851.chunk.css"
+  },
+  {
+    "revision": "a06900e632f2217ad43d",
+    "url": "/Pomodoro-clock/static/js/2.e59ed367.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-clock/static/js/2.e59ed367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39124f8a11c942c7cb78",
+    "url": "/Pomodoro-clock/static/js/main.b9f16f8e.chunk.js"
+  },
+  {
+    "revision": "aefbb190f4f576b0a28c",
+    "url": "/Pomodoro-clock/static/js/runtime-main.d2b6829f.js"
+  }
+]);
